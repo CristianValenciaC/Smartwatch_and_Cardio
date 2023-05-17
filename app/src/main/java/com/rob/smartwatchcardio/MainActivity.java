76640debfddc30b3dbe5571b80577e6e592a03d1
a901271 <diegojose.juarez.garcia@gmail.com>
2023-05-17 11:12:21 +0200

    version 0.1

--- app/src/main/java/com/rob/smartwatchcardio/MainActivity.java
+++ app/src/main/java/com/rob/smartwatchcardio/MainActivity.java
@@ -2,13 +2,37 @@ package com.rob.smartwatchcardio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button signIn;
+    private Button signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signIn=findViewById(R.id.signInButton);
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"inicio",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this,RegistroCuenta.class);
+                startActivity(i);
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/rob/smartwatchcardio/RegistroCuenta.java
@@ -0,0 +1,14 @@
+package com.rob.smartwatchcardio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistroCuenta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_cuenta);
+    }
+}
