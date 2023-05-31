1b5b6e84542b0a04d1ae14ba27102b0c749308f8
Cristian <criscod20031@gmail.com>
2023-05-31 23:59:19 +0200

    Creacion Actividad Autorizacion

---
+++ app/src/main/java/com/rob/smartwatchcardio/authorizationwatch/AuthorizationComprobate.java
@@ -0,0 +1,16 @@
+package com.rob.smartwatchcardio.authorizationwatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rob.smartwatchcardio.R;
+
+public class AuthorizationComprobate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authorization_comprobate);
+    }
+}
---
+++ app/src/main/java/com/rob/smartwatchcardio/authorizationwatch/AuthorizationFalse.java
@@ -0,0 +1,16 @@
+package com.rob.smartwatchcardio.authorizationwatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rob.smartwatchcardio.R;
+
+public class AuthorizationFalse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authorization_false);
+    }
+}
