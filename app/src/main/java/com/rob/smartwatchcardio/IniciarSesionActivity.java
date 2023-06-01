5e2a57c796a39e7de8c1cae2bf2e8d062cc7f179
SebHoet <sebastianhoetd@gmail.com>
2023-06-01 22:08:51 +0200

    Pegandome con tablas

--- app/src/main/java/com/rob/smartwatchcardio/IniciarSesionActivity.java
+++ app/src/main/java/com/rob/smartwatchcardio/IniciarSesionActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +17,16 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class IniciarSesionActivity extends AppCompatActivity {
 
@@ -24,10 +35,13 @@ public class IniciarSesionActivity extends AppCompatActivity {
     ProgressBar progressBar;
     TextView registerBtnTextView;
 
+    private DatabaseReference database;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iniciar_sesion);
+
+        database = FirebaseDatabase.getInstance("https://smartwatch-and-cardio-default-rtdb.europe-west1.firebasedatabase.app/").getReference();
 
         emailEditText = findViewById(R.id.email_edit_text);
         passwordEditText = findViewById(R.id.password_edit_text);
@@ -68,6 +82,7 @@ public class IniciarSesionActivity extends AppCompatActivity {
                     if(firebaseAuth.getCurrentUser().isEmailVerified()){
 
                         //Ir al inicio de la aplicacion
+                        guardarUsuario(firebaseAuth);
                         startActivity(new Intent(IniciarSesionActivity.this, InicioPrincipal.class));
 
                     }else{
@@ -121,5 +136,89 @@ public class IniciarSesionActivity extends AppCompatActivity {
         }
 
         return true;
+    }
+    void guardarUsuario(FirebaseAuth firebaseAuth){
+        FirebaseUser usuario = firebaseAuth.getCurrentUser();
+        String uidUsuario = usuario.getUid();
+
+        database.child("Usuario").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+               boolean existe = false;
+                //verificar si existe usuarios
+               if (dataSnapshot.exists()){
+                    //buscar usuario para saber si existe
+                   existeUsuarioBase(uidUsuario);
+                  Log.i("base",  "a");
+
+                   /*esto funciona para crear pero no llega a reconocer que existe ya
+                   * for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                       String uid = ds.child("uid").getValue().toString();
+                        // si existe lo detenemos
+                       if (uid == uidUsuario){
+                           Log.i("base", "usuario existe");
+                          existe= true;
+                          break;
+                       }
+
+                   }
+                   if (!existe){
+                       Log.i("base", "No existe usuario, creando");
+                       Map<String, Object> usuario = new HashMap<>();
+                       usuario.put("nombre","manolo");
+                       usuario.put("apellido","dominguez");
+                       usuario.put("uid", uidUsuario);
+                       database.child("Usuario").push().setValue(usuario);
+                   }
+*/
+
+
+
+
+               } else {
+                   Log.i("base", "directamente no existen usuarios, creando primero");
+                   Map<String, Object> usuario = new HashMap<>();
+                   usuario.put("nombre","manolo");
+                   usuario.put("apellido","dominguez");
+                   usuario.put("uid", uidUsuario);
+                    database.setValue("Usuario");
+                   database.child("Usuario").push().setValue(usuario);
+               }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+    }
+    void existeUsuarioBase(String uidUsuario){
+        Query query = database.child("Usuario").orderByChild("uid").equalTo(uidUsuario).limitToFirst(1);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.i("caso", "curioso");
+                if (dataSnapshot.exists()) {
+                    Log.i("existe", "si");
+                    // El resultado de la consulta se encuentra en el dataSnapshot
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+
+                    }
+                } else {
+                    // No se encontraron resultados
+                    System.out.println("No se encontraron resultados");
+                    Log.i("existe", "no");
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Se produjo un error al realizar la consulta
+                System.out.println("Error al realizar la consulta: " + databaseError.getMessage());
+            }
+        });
     }
 }
