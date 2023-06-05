08efaaa609d9922043323124ecb22085787e9bfa
NGonzMar <nuriagonzalezmargallo@gmail.com>
2023-06-05 21:08:26 +0200

    Logout implementado

--- app/src/main/java/com/rob/smartwatchcardio/ui/perfilFragment/PerfilFragment.java
+++ app/src/main/java/com/rob/smartwatchcardio/ui/perfilFragment/PerfilFragment.java
@@ -1,20 +1,26 @@
 package com.rob.smartwatchcardio.ui.perfilFragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageButton;
+import androidx.appcompat.widget.PopupMenu;
+import com.google.firebase.auth.FirebaseAuth;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.rob.smartwatchcardio.IniciarSesionActivity;
+import com.rob.smartwatchcardio.R;
 import com.rob.smartwatchcardio.databinding.FragmentPerfilBinding;
 
 public class PerfilFragment extends Fragment {
 
     private FragmentPerfilBinding binding;
+    ImageButton menuBtn;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,7 +30,27 @@ public class PerfilFragment extends Fragment {
         binding = FragmentPerfilBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        menuBtn = root.findViewById(R.id.menu_btn);
+        menuBtn.setOnClickListener((v) -> showMenu());
+
         return root;
+    }
+
+    void showMenu() {
+        PopupMenu popupMenu = new PopupMenu(requireContext(), menuBtn);
+        popupMenu.getMenu().add("Cerrar sesión");
+        popupMenu.show();
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                if (menuItem.getTitle().equals("Cerrar sesión")) {
+                    FirebaseAuth.getInstance().signOut();
+                    startActivity(new Intent(requireContext(), IniciarSesionActivity.class));
+                    requireActivity().finish();
+                }
+                return false;
+            }
+        });
     }
 
     @Override
