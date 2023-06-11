c565ea81f6f7a1ef963c7803632418a32a42cf0d
Cristian <criscod20031@gmail.com>
2023-06-11 16:28:03 +0200

    Solución otro error

--- app/src/main/java/com/rob/smartwatchcardio/RegistroCuenta.java
+++ app/src/main/java/com/rob/smartwatchcardio/RegistroCuenta.java
@@ -28,6 +28,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.rob.smartwatchcardio.authorizationwatch.Authorization;
 
 import java.util.HashMap;
 import java.util.Map;
